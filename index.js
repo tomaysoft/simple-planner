12cf8543ff32e3959d3d43713dc423e2c66157aa
tomaysoft <44364462+tomaysoft@users.noreply.github.com>
2024-03-07 00:10:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ class Scheduler {
         if (participants.length === 0) {
             return "Error - Participants list is empty.";
         }
+        const scheduledBusyTimeSlots = this.meetings.map((meeting) => meeting.meetingStartTime);
+        if (this.meetings.filter((meeting) => meeting.meetingStartTime.getTime() === startTime.getTime()).length > 0) {
+            return "Error - this timeslot is already busy.";
+        }
         const meeting = { meetingParticipants: participants, meetingStartTime: startTime };
         this.meetings.push(meeting);
         return meeting;
@@ -20,12 +24,12 @@ class Scheduler {
     listPlannedMeetingsForUser(person) {
         return this.meetings.filter((meeting) => meeting.meetingParticipants.some((participant) => participant.personEmail === person.personEmail));
     }
-    checkAvailableTimeSlotsForUsers(participants) {
+    checkAvailableTimeSlotsForUsers(participants, dateToCheck) {
         const scheduledBusyTimeSlots = this.meetings.map((meeting) => meeting.meetingStartTime);
         const availableSlots = [];
         // working hours are from 8 AM to 6 PM
         for (let hour = 8; hour < 18; hour++) {
-            const potentialSlot = new Date('2024-03-05');
+            const potentialSlot = dateToCheck;
             potentialSlot.setHours(hour, 0, 0, 0);
             if (!scheduledBusyTimeSlots.some((busySlot) => busySlot.getTime() === potentialSlot.getTime())) {
                 availableSlots.push(new Date(potentialSlot));
@@ -56,9 +60,12 @@ console.log('meeting not in hour mark', meeting4);
 const meeting5Time = new Date(scheduleDate.setHours(15, 0, 0, 0));
 const meeting5 = scheduler.defineNewMeeting([], meeting5Time);
 console.log('meeting with no participants', meeting5);
+const meeting6Time = new Date(scheduleDate.setHours(14, 0, 0, 0));
+const meeting6 = scheduler.defineNewMeeting([userJohn, userJane], meeting6Time);
+console.log('meeting on busy time', meeting6);
 const johnsSchedule = scheduler.listPlannedMeetingsForUser(userJohn);
 console.log('schedule for John', johnsSchedule);
 const janesSchedule = scheduler.listPlannedMeetingsForUser(userJane);
 console.log('schedule for Jane', janesSchedule);
-const availableSlots = scheduler.checkAvailableTimeSlotsForUsers([userJohn, userJane]);
+const availableSlots = scheduler.checkAvailableTimeSlotsForUsers([userJohn, userJane], scheduleDate);
 console.log('available time slots', availableSlots);
